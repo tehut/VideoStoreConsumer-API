c6bfd1ad8b6a9bbfe9912659622db22dc4a80d74
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-06-16 09:54:27 -0700

    added create to movie routes, create to movies_controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,18 @@ class MoviesController < ApplicationController
     render status: :ok, json: data
   end
 
+  def create
+    movie = Movie.new(movie_params)
+    puts params[:movie].to_hash
+    if movie.save
+      render status: :ok, json: { id: movie.id }
+    else
+      render status: :bad_request, json: { errors: movie.errors.messages }
+    end
+    # movie = Movie.create(movie_params)
+    # redirect_to movies_path
+  end
+
   def show
     render(
       status: :ok,
@@ -28,5 +40,9 @@ class MoviesController < ApplicationController
     unless @movie
       render status: :not_found, json: { errors: { title: ["No movie with title #{params["title"]}"] } }
     end
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :inventory, :overview, :release_date, :image_url)
   end
 end
